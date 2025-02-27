b1ca9a0d092090e0e2bf482cc866d6c30dd247b7
Neville Khabuchi <nevillekhabuchi@gmail.com>
2025-02-27 22:53:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBars from './Navbars';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return <AppContainer/>
 }
 
-export default App;
+function AppContainer() {
+  return (
+    <div className='appContainer'>
+      <ContainerLeft/>
+      <ContainerRight/>
+    </div>
+  )
+}
+
+function ContainerLeft() {
+  return (
+    <div className='containerLeft'>
+
+    </div>
+  )
+}
+
+function ContainerRight() {
+  return (
+    <div className='containerRight'>
+        <NavBars />
+    </div>
+  )
+}
---
+++ src/Navbars.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBars() {
+    return (
+        <div className="top">
+            <p>ABOUT</p>
+            <p>RESUME</p>
+            <p>PORTFOLIO</p>
+            <p>CONTACT</p>
+        </div>
+    )
+}
+export default NavBars;
